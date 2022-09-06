5487c012f65d56e66d7920bcfb134c027c706dd9
AltF4VED <petr0304@bk.ru>
2022-09-06 14:19:18 +0300

    Добавлена сущность "Производитель"

--- FoodStoreApp/Data/ApplicationDbContext.cs
+++ FoodStoreApp/Data/ApplicationDbContext.cs
@@ -9,6 +9,6 @@ namespace FoodStoreApp.Data
         {
         }
         public DbSet<Category> Category { get; set; }
-        //public DbSet<Manufacturers> Manufacturers {get; set; }
+        public DbSet<Manufacturers> Manufacturers {get; set; }
     }
 }
